1c96a411186b74011b62088917c57a1be45bee39
Deng Zhenxiang <dengzhenxiang@haylion.cn>
2019-01-03 21:22:08 +0800

    新建工程

---
+++ family-edu/src/main/java/com/senb/familyedu/FamilyEduApplication.java
@@ -0,0 +1,14 @@
+package com.senb.familyedu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilyEduApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FamilyEduApplication.class, args);
+    }
+
+}
+
