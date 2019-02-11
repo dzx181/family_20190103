cf2e344b7065ec9b7579e742d5aebb38d7492590
Deng Zhenxiang <dengzhenxiang@haylion.cn>
2019-02-11 16:49:50 +0800

    add 4.txt

--- family-edu/src/main/java/com/senb/familyedu/FamilyEduApplication.java
+++ family-edu/src/main/java/com/senb/familyedu/FamilyEduApplication.java
@@ -3,7 +3,7 @@ package com.senb.familyedu;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//启动类
 @SpringBootApplication
 @MapperScan(basePackages = "com.senb.familyedu.dao")
 public class FamilyEduApplication {
